cb02b61aee35a46d4ce924363a67ff86387a4e00
bourbonboi <artemmine9292@gmail.com>
2024-10-01 18:25:39 +0900

    zadanie 12

--- hello_world.py
+++ hello_world.py
@@ -1,12 +1,7 @@
-import random
-
 def main():
     name = input("Введите имя: ")
     print("Hello world from", name)
 
 if __name__ == "__main__":
     main()
-    input()
 
-
-# Новая версия программы
